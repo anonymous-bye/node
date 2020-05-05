0d5dec6690fcf6b75632261e10bc2c8e550f08e2
anonymous-bye <63511442+anonymous-bye@users.noreply.github.com>
2020-05-05 22:26:56 +0900

    Update all.js

--- p/all.js
+++ p/all.js
@@ -4,10 +4,7 @@ function FindProxyForURL(url, host)
   if (shExpMatch(host, "*skype*"))
     return "DIRECT";
   
-  if (shExpMatch(host, "www.google*"))
-    return "DIRECT";
-  
-  if (shExpMatch(host, "play.google*"))
+  if (shExpMatch(host, "*.google.*"))
     return "DIRECT";
 
   return "SOCKS5 54.65.39.97:80";
