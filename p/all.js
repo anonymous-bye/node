0a93748144d00339f7a8052154d9e8fd714e87d9
anonymous-bye <63511442+anonymous-bye@users.noreply.github.com>
2020-05-26 12:12:58 +0900

    Update all.js

--- p/all.js
+++ p/all.js
@@ -9,6 +9,9 @@ function FindProxyForURL(url, host)
   
   if (shExpMatch(host, "*bitmex*"))
     return "DIRECT";
+  
+  if (shExpMatch(host, "*.blockchain.*"))
+    return "DIRECT";
 
   return "SOCKS5 54.168.8.51:80";
 
